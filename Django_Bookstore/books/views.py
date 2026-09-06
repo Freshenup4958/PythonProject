a8675ae81e89fbf848e6fc40ade683e3c57a0051
A27nastasi <nasti4958@gmail.com>
2026-09-06 19:34:14 +0300

    Add Django class-based views and templates

--- Django_Bookstore/books/views.py
+++ Django_Bookstore/books/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from .models import Book, Category
 
-# Create your views here.
+class BookListView(ListView):
+    model = Book
+    template_name = 'books/book_list.html'
+    context_object_name = 'books'
+    paginate_by = 3
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category = self.request.GET.get('category')
+
+        if category:
+            queryset = queryset.filter(category_id=category)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['selected_category'] = self.request.GET.get('category')
+        return context
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+    context_object_name = 'book'
+
+class BookCreateView(CreateView):
+    model = Book
+    template_name = 'books/book_form.html'
+    fields = ['category', 'title', 'author', 'price', 'description', 'stock']
+    success_url = '/'
+
+class BookUpdateView(UpdateView):
+    model = Book
+    template_name = 'books/book_form.html'
+    fields = ['category', 'title', 'author', 'price', 'description', 'stock']
+    success_url = '/'
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'books/book_confirm_delete.html'
+    success_url = '/'
